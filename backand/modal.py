ff97550147a8b0d6c535cbee3a15133199625a4f
Chumak Vladimir <parsival427@gmail.com>
2024-05-17 22:33:52 +0300

    Добавил метод удаления шума

---
+++ backand/modal.py
@@ -0,0 +1,16 @@
+from rnnoise_wrapper import RNNoise
+
+
+def transribation(audio_file_name):
+    noise_audio_file_name  = _remove_noise(audio_file_name)
+
+def _remove_noise(audio_file_name):
+    denoiser = RNNoise()
+    audio = denoiser.read_wav(audio_file_name)
+    denoised_audio = denoiser.filter(audio)
+    noise_audio_file_name = f"rm_noise_{audio_file_name}"
+    denoiser.write_wav(noise_audio_file_name, denoised_audio)
+    return noise_audio_file_name
+
+
+
--- home.py
+++ home.py
@@ -1,8 +1,11 @@
 import datetime
+import io
 from enum import Enum
 from typing import Union, List, Optional
 from fastapi import FastAPI, UploadFile, File
 from pydantic import BaseModel, Field
+from pydub import AudioSegment
+from backand import modal
 
 
 app = FastAPI(
@@ -17,9 +20,18 @@ def init():
 
 @app.post("/audio")
 async def upload_audio(file: UploadFile):
-    content = await file.read()
-    with open(f"uploaded_{file.filename}", "wb") as f:
-        f.write(content)
+    answer = None
+    file_name = file.filename.lower()
+    if file_name.endswith('.mp3'):
+        answer =  'mp3'
+    elif file_name.endswith('.wav'):
+        answer = 'wav'
+        file_content = await file.read()
+        with open(f"{file_name}", "wb") as f:
+            f.write(file_content)
+        modal.transribation(file_name)
+
     return {
-        "correct":True
+        "file_name":file_name,
+        "answer":answer
     }
