a08a8cf3c4f8930c970bcc370dce27cf7812bd9b
Alexsandr52 <ak.polyanskiy@gmail.com>
2024-05-18 20:49:54 +0300

    fix qcheck

--- qcheck.py
+++ qcheck.py
@@ -1,21 +1,12 @@
 from fuzzywuzzy import process
 
-# Данные
-data = {
-    "0": "на утро десять",
-    "1": "",
-    "2": "понятно опе по первому пути на станции населка",
-    "3": "второй, радельно оператор перегольный красногвардей, загромная здравствуйте, не затягиваетесь, хорошо, до станции сорочинская проедьте пожалуйста по тоцкой, по первому пути будете ехать до мц барного",
-    "4": "а шиленкой на поездной ой на маникрую переходите на окав"
-}
-
 str_count = 2  
 count_first_words = 2  
 
-target_words = ['машинист', 'оператор']
+target_words = ['машинист', 'оператор', 'дежурный']
 
 # Функция для проверки наличия целевых слов в первых нескольких строках текста
-def check_input(dialog, target_words, str_count, count_first_words):
+def check_input_frases(dialog, target_words, str_count, count_first_words):
     target = {word: False for word in target_words}
 
     # Удаление пустых строк из диалога
@@ -37,8 +28,17 @@ def check_input(dialog, target_words, str_count, count_first_words):
 
     return target
 
-# Проверка данных
-print(check_input(data, target_words, str_count, count_first_words))
+if __name__ == '__main__':
+    # Данные
+    data = {
+        '0': 'на утро десять',
+        '1': '',
+        '2': 'понятно опе по первому пути на станции населка',
+        '3': 'второй, радельно оператор перегольный красногвардей, загромная здравствуйте, не затягиваетесь, хорошо, до станции сорочинская проедьте пожалуйста по тоцкой, по первому пути будете ехать до мц барного',
+        '4': 'а шиленкой на поездной ой на маникрую переходите на окав'
+    }
+    # Проверка данных
+    print(check_input_frases(data, target_words, str_count, count_first_words))
 
 
 
