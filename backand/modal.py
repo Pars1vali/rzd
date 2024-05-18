b9027ed050696d6a434187a2eacbadc8308ffb92
Chumak Vladimir <parsival427@gmail.com>
2024-05-18 08:54:35 +0300

    Увелечение громкости аудио

--- backand/modal.py
+++ backand/modal.py
@@ -8,12 +8,16 @@ asr_model = nemo_asr.models.EncDecRNNTBPEModel.from_pretrained("nvidia/stt_ru_co
 def transribation(audio_file):
     #Папка где хранятся файлы служебного переговора
     output_directory = "output"
+
     #очищенная от шума аудидорожка
     clear_audio = _remove_noise(audio_file)
+    #увеличить громкость голоса говорящих
+    louder_audio = _volume_up(clear_audio, 10)
     #разделение аудиодорожки на куски с речью
-    count_speech_voices = _trim_audio_speech(clear_audio, output_directory)
+    count_speech_voices = _trim_audio_speech(louder_audio, output_directory)
     #распознование речи в кусках текста
     text = _voice_recognition_audio(output_directory)
+
     return count_speech_voices, text
 
 def _remove_noise(audio_file_name):
@@ -23,6 +27,13 @@ def _remove_noise(audio_file_name):
     noise_audio_file_name = f"rm_noise_{audio_file_name}"
     denoiser.write_wav(noise_audio_file_name, denoised_audio)
     return noise_audio_file_name
+
+def _volume_up(input_file_path, increase_by_db):
+    output_file_path = f"louder_{input_file_path}"
+    audio = AudioSegment.from_file(input_file_path)
+    louder_audio = audio + increase_by_db
+    louder_audio.export(output_file_path, format="wav")
+    return output_file_path
 
 def _trim_audio_speech(clear_audio, output_directory):
     def _export_nonsilent_chunks(audio_segment, nonsilent_ranges, output_dir="output"):
@@ -36,8 +47,7 @@ def _trim_audio_speech(clear_audio, output_directory):
     _export_nonsilent_chunks(audio, nonsilent_ranges, output_dir=output_directory)
     return len(nonsilent_ranges),
 
-def _volume_up():
-    pass
+
 
 def _voice_recognition_audio(directory_path):
     def _voice_recognition(speech_file):
